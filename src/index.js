fe21ed23e27d21998a2d3e05b0369fc1e1ef7867
Vineet Tiwari <vineettiwari@Vineets-MacBook-Air.local>
2026-06-21 12:10:10 +0530

    db connection

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv"
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+
+
+/*
+const app = express()
+(async()=>{
+ try{
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+    app.on("error", (error)=>{
+        console.log("ERROR: ", error)
+        throw error
+    })
+
+    app.listen(process.env.PORT, ()=>{
+        console.log(`App is listening on port ${process.env.PORT}`)
+    })
+ }catch(err){
+    console.error("ERROR :" , err)
+    throw err
+ }
+})()
+*/ 
+
