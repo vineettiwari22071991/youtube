19289d478d90a6f0e5d9773dabbc559b288c3c0e
Vineet Tiwari <vineettiwari@Vineets-MacBook-Air.local>
2026-06-22 14:31:04 +0530

    subscriptionSchema

---
+++ src/models/subscription.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+    {
+        subscription:
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+        ,
+        channel:
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    })
+
+export const Subcription = mongoose.model("Subcription", subscriptionSchema)
