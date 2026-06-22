91d921452e18a6015055291080631dc78c77e3d1
Vineet Tiwari <vineettiwari@Vineets-MacBook-Air.local>
2026-06-22 18:41:18 +0530

    add updload functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -221,10 +221,123 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 })
 
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+
+    const {oldPassword, newPassword} = req.body
+
+
+    const user = await User.findById(req.user?._id)
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "Invalid old password")
+    }
+
+    user.password = newPassword
+
+   await user.save()
+
+   return res.status(200)
+   .json(new ApiResponse(200, {}, "Password change successfull"))
+})
+
+
+const getCurrentUser = asyncHandler(async (req, res)=> {
+    return res.status(200).json(ApiResponse(200,
+        req?.user, 
+    "Get user information successfully"
+))
+})
+
+const updateAccountDetails = asyncHandler(async (req, res)=>{
+
+    const {fullname, email} = req.body
+
+    if(!fullname || !email){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                fullname,
+                email
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res.status(200)
+    .json(new ApiResponse(200, user,"Account detail update sucessfully"))
+})
+
+const updateUserAvatar = asyncHandler (async (req, res)=>{
+
+    const avatarLocalPath = req.file?.path
+
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is missing")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if(!avatar.url){
+        throw new ApiError(400, "Error while uploading on avatar")
+    }
+
+    const user = await User.findByIdAndUpdate(req.user?._id,
+        {
+            $set:{
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res.status(200)
+    .json(new ApiResponse(200, user,"Avatar image updated"))
+})
+
+const updateUserCoverImage = asyncHandler (async (req, res)=>{
+
+    const coverImageLocalPath = req.file?.path
+
+
+    if(!coverImageLocalPath){
+        throw new ApiError(400, "coverImage file is missing")
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!coverImage.url){
+        throw new ApiError(400, "Error while uploading on coverImage")
+    }
+
+    const user = await User.findByIdAndUpdate(req.user?._id,
+        {
+            $set:{
+                coverImage: coverImage.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res.status(200)
+    .json(new ApiResponse(200, user,"CoverImage updated"))
+})
+
 
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserCoverImage,
+    updateUserAvatar
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express"
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js"
+import {
+    registerUser, loginUser, logoutUser, refreshAccessToken
+    , changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar,
+    updateUserCoverImage
+} from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 const router = Router()
@@ -25,6 +29,21 @@ router.route("/login").post(loginUser)
 
 //secure route
 router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/get-current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-user-account").patch(verifyJWT, updateAccountDetails)
+router.route("/update-cover-image").patch(verifyJWT, upload.fields(
+    {
+        name: coverImage,
+        maxCount: 1
+    }
+), updateUserCoverImage)
+router.route("/update-avatar").patch(verifyJWT, upload.fields(
+    {
+        name: avatar,
+        maxCount: 1
+    }
+), updateUserAvatar)
 
 router.route("/refresh-token").post(refreshAccessToken)
 
